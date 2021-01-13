1b43e1d806f8f39a405dd8e2ce4ae035c7836760
rizkypp <77381783+rizkypp@users.noreply.github.com>
2021-01-13 20:16:42 +0700

    Create auth_footer.php

---
+++ KantorPos/application/views/auth/auth_footer.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+<!-- Bootstrap core javascript -->
+	<script src="<?php echo base_url().'assets/templates/admin/vendor/jquery/jquery.min.js'?>"></script>
+	<script src="<?php echo base_url().'assets/templates/admin/vendor/bootstrap/js/bootstrap.bundle.min.js'?>"></script>
+
+	<!-- core plugin javascript -->
+	<script src="<?php echo base_url().'assets/templates/admin/vendor/jquery-easing/jquery.easing.min.js'?>"></script>
+
+	<!-- custom scripts for all pages -->
+	<script src="<?php echo base_url().'assets/templates/admin/js/sb-admin-2.min.js'?>"></script>
