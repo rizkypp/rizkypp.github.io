f8bd7f5060c60477b257076576f095a13620ef6b
rizkypp <77381783+rizkypp@users.noreply.github.com>
2021-01-13 20:16:45 +0700

    Create auth_head.php

---
+++ KantorPos/application/views/auth/auth_head.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Admin Pos | Login</title>
+
+	<!-- Custom Fonts for this template -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(). 'assets/templates/admin/vendor/fontawesome-free/css/all.min.css'?>">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+
+	<!-- Custom style for this template-->
+	<link rel="stylesheet" href="<?php echo base_url().'assets/templates/admin/css/sb-admin-2.min.css'?>">
+</head>
