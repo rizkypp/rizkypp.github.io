39bb83226a37796e540df228e327a5c05bf086a0
rizkypp <77381783+rizkypp@users.noreply.github.com>
2021-01-13 20:18:47 +0700

    Create head.php

---
+++ KantorPos/application/views/template/head.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset= utf-8">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=divece-width, initial-scale=1">
+
+	<title>Monitoring |</title>
+
+	<!-- Custom fonts template -->
+	<link href="<?php echo base_url().'assets/templates/admin/vendor/fontawesome-free/css/all.min.css'?>" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+	<!-- Custom styles Template -->
+	<link href="<?php echo base_url().'assets/templates/admin/css/sb-admin-2.min.css'?>" rel="stylesheet">
+</head>
