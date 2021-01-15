c7e2303f39e79a14a243b9ac3467c4e6a087d649
rizkypp <77381783+rizkypp@users.noreply.github.com>
2021-01-15 16:08:19 +0700

    Create MainActivity.java

---
+++ Project/app/src/main/java/com/pos/project/MainActivity.java
@@ -0,0 +1,21 @@
+package com.pos.project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void map(View view) {
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+        //finish();
+        startActivity(intent);
+    }
+}
